a23240907b6401b6bb5fdbf55b9a2875e4898435
kevinoon <ripplitt@protonmail.com>
2026-01-10 09:00:39 -0800

    Set up for next bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveBot.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name = "Tele", group = "Linear Opmode")
+public class Tele extends LinearOpMode {
+    
+}
